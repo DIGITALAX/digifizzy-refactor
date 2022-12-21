c5e54418d6110b0940981df2d05d640a3574a9c6
emmajane1313 <admin@digitalax.xyz>
2022-12-21 14:46:20 -0500

    fix url search

--- components/Home/Collection.tsx
+++ components/Home/Collection.tsx
@@ -6,8 +6,8 @@ import { useMediaQuery } from "@material-ui/core";
 const Collection: FunctionComponent = (): JSX.Element => {
   const iframes: string[] = ["yt1", "yt2"];
   const links: string[] = [
-    "https://www.youtube.com/embed/__PtdR1xZYY",
-    "https://www.youtube.com/embed/CqpU5vCQxGM",
+    "https://www.youtube.com/embed/Jtsfk7u6Mxw",
+    "https://www.youtube.com/embed/ntzmHYiy2Ko",
   ];
   let queryWindowSizeMobile: boolean = useMediaQuery("(max-width:660px)");
   let queryWindowSizeGalaxy: boolean = useMediaQuery("(max-width:300px)");
